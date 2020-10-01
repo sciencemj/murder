53d141355e6a1284821a5548202497dac24e242e
sciencemj <sciencemj.park@gmail.com>
2020-10-01 13:49:00 +0900

    gunp spawn bug fixed

--- src/murder/Main.java
+++ src/murder/Main.java
@@ -58,41 +58,51 @@ public class Main extends JavaPlugin implements Listener {
 
     Location randomLocation(Location min, Location max)
     {
-        Location range = new Location(min.getWorld(), Math.abs(max.getX() - min.getX()), min.getY(), Math.abs(max.getZ() - min.getZ()));
-        return new Location(min.getWorld(), (Math.random() * range.getX()) + (min.getX() <= max.getX() ? min.getX() : max.getX()), range.getY(), (Math.random() * range.getZ()) + (min.getZ() <= max.getZ() ? min.getZ() : max.getZ()));
+        Location range = new Location(min.getWorld(), Math.abs(max.getX() - min.getX()), Math.abs(max.getY() - min.getY()), Math.abs(max.getZ() - min.getZ()));
+        return new Location(min.getWorld(), (Math.random() * range.getX()) + (min.getX() <= max.getX() ? min.getX() : max.getX()), (Math.random() * range.getY()) + (min.getY() <= max.getY() ? min.getY() : max.getY()), (Math.random() * range.getZ()) + (min.getZ() <= max.getZ() ? min.getZ() : max.getZ()));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player s = (Player) sender;
-            if (command.getName().equals("murder")) {
-                if (args.length > 0) {
+
+        if (command.getName().equals("murder")) {
+            if (args.length > 0) {
+                if (sender instanceof Player) {
+                    Player s = (Player) sender;
                     if (args[0].equals("set1")) {
                         s.sendMessage("pos1 set");
                         loc1 = s.getLocation();
                     } else if (args[0].equals("set2")) {
                         s.sendMessage("pos2 set");
                         loc2 = s.getLocation();
+                    } else if (args[0].equals("stop")) {
+                        survivors = 0;
+                        gameSet();
                     }
-                } else {
-                    Player[] ps = ((Collection<Player>) Bukkit.getOnlinePlayers()).toArray(new Player[0]);
-                    Random r = new Random();
-                    game = true;
-                    survivors = 1;
-                    murderer = ps[r.nextInt(ps.length)];
-                    murderer.sendTitle(ChatColor.RED + "머더입니다", "", 20, 20, 0);
+                }else {
+                    if (args[0].equals("set1")) {
+                        loc1 = new Location(Bukkit.getOnlinePlayers().toArray(new Player[0])[0].getWorld(), Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]));
+                    } else if (args[0].equals("set2")) {
+                        loc2 = new Location(Bukkit.getOnlinePlayers().toArray(new Player[0])[0].getWorld(), Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]));
+                    }
+                }
+            } else {
+                Player[] ps = ((Collection<Player>) Bukkit.getOnlinePlayers()).toArray(new Player[0]);
+                Random r = new Random();
+                game = true;
+                survivors = 1;
+                murderer = ps[r.nextInt(ps.length)];
+                murderer.sendTitle(ChatColor.RED + "머더입니다", "", 20, 20, 0);
+                gunner = ps[r.nextInt(ps.length)];
+                while (murderer == gunner && ps.length != 1)
                     gunner = ps[r.nextInt(ps.length)];
-                    while (murderer == gunner && ps.length != 1)
-                        gunner = ps[r.nextInt(ps.length)];
-                    gunner.sendTitle(ChatColor.GREEN + "총잡이입니다", "", 20, 20, 0);
-                    gunner.getInventory().addItem(gun);
-                    murderer.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1));
-                    for (Player p : ps) {
-                        if (p != murderer && p != gunner) {
-                            survivors++;
-                            p.sendTitle(ChatColor.GREEN + "시민입니다", "", 20, 20, 0);
-                        }
+                gunner.sendTitle(ChatColor.GREEN + "총잡이입니다", "", 20, 20, 0);
+                gunner.getInventory().addItem(gun);
+                murderer.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1));
+                for (Player p : ps) {
+                    if (p != murderer && p != gunner) {
+                        survivors++;
+                        p.sendTitle(ChatColor.GREEN + "시민입니다", "", 20, 20, 0);
                     }
                 }
             }
@@ -133,55 +143,57 @@ public class Main extends JavaPlugin implements Listener {
     public void playerInteract(PlayerInteractEvent e){
         Player p = e.getPlayer();
         if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
-            if (p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(gun.getItemMeta().getDisplayName())){
-                Vector loc = p.getLocation().getDirection().multiply(0.5f);
-                Location block = p.getEyeLocation();
-                //block.add(0, 1.5, 0);
-                for (int i = 0; i < 40; i++) { // 20block range
-                    block = new Vector(block.getX() + loc.getX(), block.getY() + loc.getY(), block.getZ() + loc.getZ()).toLocation(p.getWorld());
-                    if (!block.getBlock().getType().equals(Material.AIR)) {
-                        //block.getWorld().createExplosion(block,0.1f);
-                        break;
-                    }
-                    Collection<Entity> entities = block.getWorld().getNearbyEntities(block, 0.5D, 0.5D, 0.5D);
-                    for (Entity entity : entities) {
-                        if (entity != p) {
-                            if (entity instanceof Player) {
-                                Player t = (Player) entity;
-                                if (t.getGameMode() != GameMode.SPECTATOR) {
-                                    Block head = t.getLocation().getBlock();
-                                    head.setType(Material.PLAYER_HEAD);
-                                    BlockState state = head.getState();
-                                    Skull skull = (Skull) state;
-                                    UUID uuid = t.getUniqueId();
-                                    skull.setOwningPlayer(Bukkit.getServer().getOfflinePlayer(uuid));
-                                    skull.update();
-                                    t.setGameMode(GameMode.SPECTATOR);
-                                    if (t == murderer) {
-                                        for (Player player : Bukkit.getOnlinePlayers()) {
-                                            player.sendTitle(ChatColor.GREEN + "시민 승리!!", "", 20, 20, 0);
+            if (p.getInventory().getItemInMainHand().getItemMeta() != null) {
+                if (p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(gun.getItemMeta().getDisplayName())) {
+                    Vector loc = p.getLocation().getDirection().multiply(0.5f);
+                    Location block = p.getEyeLocation();
+                    //block.add(0, 1.5, 0);
+                    for (int i = 0; i < 40; i++) { // 20block range
+                        block = new Vector(block.getX() + loc.getX(), block.getY() + loc.getY(), block.getZ() + loc.getZ()).toLocation(p.getWorld());
+                        if (!block.getBlock().getType().equals(Material.AIR)) {
+                            //block.getWorld().createExplosion(block,0.1f);
+                            break;
+                        }
+                        Collection<Entity> entities = block.getWorld().getNearbyEntities(block, 0.5D, 0.5D, 0.5D);
+                        for (Entity entity : entities) {
+                            if (entity != p) {
+                                if (entity instanceof Player) {
+                                    Player t = (Player) entity;
+                                    if (t.getGameMode() != GameMode.SPECTATOR) {
+                                        Block head = t.getLocation().getBlock();
+                                        head.setType(Material.PLAYER_HEAD);
+                                        BlockState state = head.getState();
+                                        Skull skull = (Skull) state;
+                                        UUID uuid = t.getUniqueId();
+                                        skull.setOwningPlayer(Bukkit.getServer().getOfflinePlayer(uuid));
+                                        skull.update();
+                                        t.setGameMode(GameMode.SPECTATOR);
+                                        if (t == murderer) {
+                                            for (Player player : Bukkit.getOnlinePlayers()) {
+                                                player.sendTitle(ChatColor.GREEN + "시민 승리!!", "", 20, 20, 0);
+                                            }
+                                            game = false;
+                                            break;
+                                        } else {
+                                            survivors--;
+                                            gameSet();
+                                            break;
                                         }
-                                        game = false;
-                                        break;
-                                    } else {
-                                        survivors--;
-                                        gameSet();
-                                        break;
                                     }
                                 }
                             }
                         }
+                        p.getWorld().spawnParticle(Particle.COMPOSTER, block, 1, 0, 0, 0, 0);
                     }
-                    p.getWorld().spawnParticle(Particle.COMPOSTER, block, 1, 0, 0, 0, 0);
-                }
-                p.getWorld().playSound(p.getLocation(), Sound.BLOCK_ANVIL_HIT, 1, 1);
-                p.getInventory().removeItem(gun);
-            }else if (p.getInventory().getItemInMainHand().getType().equals(gunp.getType())){
-                if (p.getInventory().containsAtLeast(gunp, 10)){
-                    gunp.setAmount(10);
-                    p.getInventory().removeItem(gunp);
-                    gunp.setAmount(1);
-                    p.getInventory().addItem(gun);
+                    p.getWorld().playSound(p.getLocation(), Sound.BLOCK_ANVIL_HIT, 1, 1);
+                    p.getInventory().removeItem(gun);
+                } else if (p.getInventory().getItemInMainHand().getType().equals(gunp.getType())) {
+                    if (p.getInventory().containsAtLeast(gunp, 10)) {
+                        gunp.setAmount(10);
+                        p.getInventory().removeItem(gunp);
+                        gunp.setAmount(1);
+                        p.getInventory().addItem(gun);
+                    }
                 }
             }
         }
@@ -205,12 +217,12 @@ public class Main extends JavaPlugin implements Listener {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 player.sendTitle(ChatColor.RED + "머더 승리!", "", 20, 20, 0);
             }
-            World world = getServer().getWorld("world");//get the world
+            World world = murderer.getWorld();//get the world
             List<Entity> entList = world.getEntities();//get all entities in the world
 
             for(Entity current : entList) {//loop through the list
                 if (current instanceof Item) {
-                    if (((Item) current).getItemStack().equals(gunp))
+                    if (((Item) current).getItemStack().getType().equals(gunp.getType()))
                         current.remove();
                 }
             }
